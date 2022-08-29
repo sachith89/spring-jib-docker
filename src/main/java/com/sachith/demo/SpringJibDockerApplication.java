dcb3c8477c489777e19a67f40c5884ffaea2ef47
Sachith <sachithlakmal@icloud.com>
2022-08-29 22:30:15 +0530

    bootstrap

---
+++ src/main/java/com/sachith/demo/SpringJibDockerApplication.java
@@ -0,0 +1,13 @@
+package com.sachith.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJibDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJibDockerApplication.class, args);
+	}
+
+}
